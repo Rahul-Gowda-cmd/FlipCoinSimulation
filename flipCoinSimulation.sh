ae51184e3d77f0b49ececfbd6d322d316efd1f07
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-07-25 11:11:31 +0530

    Added: display flipCoin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,1 +1,17 @@
-#Welcome to flipCoinSimulation
+#!/bin/bash
+echo "Welcome to the Flip coin simulation program"
+echo "toss time"
+
+
+tails=0;
+heads=0;
+
+TossCheck=$((RANDOM%2));
+	if [ $TossCheck -eq 0 ];
+	then
+		tails=$(($tails+1));
+		echo Tails;
+	else
+		heads=$(($heads+1));
+		echo Heads;
+	fi
